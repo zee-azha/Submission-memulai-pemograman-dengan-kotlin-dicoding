6e4f46c92bbfc708a9f11cd40bb4cb84c31e745c
Zeezha <mohammadfaizazhary@gmail.com>
2021-07-18 15:05:07 +0700

    Memulai pemograman dasar kotlin

---
+++ Data Class dan Collections/Latihan/latihan/src/Task.kt
@@ -0,0 +1,29 @@
+/**
+ * Untuk menyelesaikan tugas latihan, Anda tidak diperbolehkan mengubah struktur kode yang sudah ada. Kecuali:
+ *    - Untuk melakukan improvisasi kode
+ *    - Mengikuti perintah yang ada
+ *
+ * Cukup tambahkan kode berdasarkan perintah yang sudah ditentukan.
+ *
+ */
+
+fun main() {
+
+    // TODO 1
+    val vehicle = mapOf<String, String>( "type" to "Motorcycle", "maxSpeed" to "230Km/s", "maxTank" to "10Ltr"
+    )
+
+    // TODO 2
+    val type = vehicle.getValue("type")
+    val maxSpeed = vehicle.getValue("maxSpeed")
+    val maxTank = vehicle.getValue("maxTank")
+
+    // TODO 3
+    println("""
+        Vehicle
+        Type: Motorcycle
+        Maximal Speed: 230Km/s
+        Maximal Tank: 10Ltr
+    """.trimIndent())
+
+}
